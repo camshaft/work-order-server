0f6337ab22b8fc95427cad49f6a2bdb0b5f677ee
Phil Harvey <philharveyx@gmail.com>
2013-09-19 12:00:17 -0500

    Render collection of work orders

--- src/workorder_root_handler.erl
+++ src/workorder_root_handler.erl
@@ -1,17 +1,48 @@
 -module(workorder_root_handler).
 
 -export([init/3]).
+-export([rest_init/2]).
+-export([service_available/2]).
 -export([content_types_provided/2]).
 -export([work_order_collection_to_json/2]).
+
+-include("workorder.hrl").
+
+-record(state, {
+  conn,
+  work_orders
+}).
 
 init(_Transport, _Req, []) ->
   {upgrade, protocol, cowboy_rest}.
 
+rest_init(Req, _Opts) ->
+  {ok, Req, #state{}}.
+
+service_available(Req, State) ->
+  case riakou:do(get_index_eq, [?JOBS_BUCKET, {binary_index, "waiting"}, <<1>>]) of
+    {ok, {keys, Keys}} -> {true, Req, State#state{work_orders = Keys}};
+    _Error ->
+      io:format("~p~n", [_Error]),
+      {false, Req, State}
+  end.
+
 content_types_provided(Req, State) ->
   {[
-    {<<"application/json">>, work_order_collection_to_json}
+    {<<"application/json">>, work_order_collection_to_json},
+    {<<"application/collection+json">>, work_order_collection_to_json}
   ], Req, State}.
 
-work_order_collection_to_json(Req, State) ->
-  Body = <<"{}">>,
-  {Body, Req, State}.
+work_order_collection_to_json(Req, State=#state{work_orders=WorkOrders}) ->
+  Body = to_json(WorkOrders),
+  {jsx:encode(Body), Req, State}.
+
+to_json(WorkOrders) ->
+  [
+    {<<"collection">>, [
+      {<<"items">>, [
+        [{<<"href">>, <<"/jobs/", WorkOrder/binary>>}] || WorkOrder <- WorkOrders
+      ]}
+    ]},
+    {<<"version">>, <<"1.0">>}
+  ].
