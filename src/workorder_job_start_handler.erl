4b8cc0f628af7642f472df1c2a8c68fd64809cc3
a1flecke <aaron.fleckenstein@gmail.com>
2013-09-19 15:30:38 -0400

    Added start handler

---
+++ src/workorder_job_start_handler.erl
@@ -0,0 +1,53 @@
+-module(workorder_job_start_handler).
+
+-export([init/3]).
+-export([allowed_methods/2]).
+-export([content_types_accepted/2]).
+-export([start_work_order/2]).
+-export([resource_exists/2]).
+-export([service_available/2]).
+-export([rest_init/2]).
+
+-include("workorder.hrl").
+
+-record(state, {
+  conn,
+  obj,
+  id
+}).
+
+init(_Transport, _Req, []) ->
+  {upgrade, protocol, cowboy_rest}.
+
+rest_init(Req, _Opts) ->
+  {ok, Req, #state{}}.
+
+allowed_methods(Req, State) ->
+    {[<<"POST">>], Req, State}.
+
+content_types_accepted(Req, State) ->
+  {[{{<<"application">>, <<"x-www-form-urlencoded">>, []}, start_work_order}], Req, State}.
+
+service_available(Req, State) ->
+  case riakou:take() of
+    {error, _} ->
+      {false, Req, State};
+    Pid ->
+      {true, Req, State#state{conn = Pid}}
+  end.
+
+resource_exists(Req, State = #state{conn = Pid}) ->
+  {ID, Req2} = cowboy_req:binding(id, Req, <<>>),
+  case riakc_pb_socket:get(Pid, ?JOBS_BUCKET, ID) of
+    {error, _} ->
+      {false, Req2, State};
+    {ok, Obj} ->
+      {true, Req2, State#state{obj = Obj, id = ID}}
+  end.
+
+start_work_order(Req, State) ->
+  %set secondary key on jobs object
+  UpdatedObj = workorder_riak:set_binary_index("status",<<"InProgress">>, State#state.obj),
+  ok = riakc_pb_socket:put(State#state.conn, UpdatedObj),
+  {ok, Req, State}.
+  
