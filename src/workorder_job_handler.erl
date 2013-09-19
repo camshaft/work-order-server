df6009209f955e0163b96eaf340a39105e76a220
Phil Harvey <philharveyx@gmail.com>
2013-09-19 15:54:14 -0500

    HTML representation for Job resources

--- src/workorder_job_handler.erl
+++ src/workorder_job_handler.erl
@@ -6,6 +6,7 @@
 -export([resource_exists/2]).
 -export([content_types_provided/2]).
 -export([to_json/2]).
+-export([to_html/2]).
 
 -include("workorder.hrl").
 
@@ -41,7 +42,8 @@ resource_exists(Req, State = #state{conn = Pid}) ->
 content_types_provided(Req, State) ->
   {[
     {{<<"application">>, <<"json">>, []}, to_json},
-    {{<<"application">>, <<"vnd.mogsie.work-order+json">>, []}, to_json}
+    {{<<"application">>, <<"vnd.mogsie.work-order+json">>, []}, to_json},
+    {{<<"text">>, <<"html">>, []}, to_html}
   ], Req, State}.
 
 to_json(Req, State = #state{obj = Obj, id = ID}) ->
@@ -55,13 +57,15 @@ to_json(Req, State = #state{obj = Obj, id = ID}) ->
   ],
 
   presenterl:conditional([
-    fast_key:get(<<"single-use">>, Job, true)
+    fast_key:get(<<"permanent">>, Job, false
+      )
   ], [
     {<<"status">>, cowboy_base:resolve([<<"jobs">>, ID, <<"status">>], Req)}
   ], P),
 
   presenterl:conditional([
-    fast_key:get(<<"single-use">>, Job, true),
+    fast_key:get(<<"permanent">>, Job, false
+      ),
     workorder_riak:has_binary_index("status", <<"Waiting">>, Obj)
   ], [
     {<<"start">>, cowboy_base:resolve([<<"jobs">>, ID, <<"start">>], Req)}
@@ -78,3 +82,53 @@ to_json(Req, State = #state{obj = Obj, id = ID}) ->
 
   {jsx:encode(Body), Req, State}.
 
+to_html(Req, State = #state{obj = Obj, id = Id}) ->
+  Job = workorder_riak:body(Obj),
+
+  P = presenterl:create(),
+
+  P ! [
+    <<"<!DOCTYPE html>\n">>,
+    <<"<html>\n">>,
+    <<"<head><title>Work Orders</title></head>\n">>,
+    <<"<body>\n">>,
+    [<<"<h1>Job">>, Id, <<"</h1>\n">>],
+    [<<"<p>">>, fast_key:get(<<"type">>, Job), <<"</p>\n">>],
+    [<<"<p>">>, jsx:encode(fast_key:get(<<"input">>, Job)), <<"</p>\n">>],
+    [<<"<table>">>]
+  ],
+
+  presenterl:conditional([
+    fast_key:get(<<"permanent">>, Job, false)
+  ], [
+    <<"<tr><td>Status</td><td>Permanent</td></tr>">>
+  ], P),
+
+  presenterl:conditional([
+    fast_key:get(<<"permanent">>, Job, false),
+    workorder_riak:has_binary_index("status", <<"Waiting">>, Obj)
+  ], [
+    <<"<tr><td>Status</td><td>Waiting</td>">>
+  ], P),
+
+  presenterl:conditional([
+    workorder_riak:has_binary_index("status", <<"InProgress">>, Obj)
+  ], [
+    <<"<tr><td>Status</td><td>In Progress</td>">>
+  ], P),
+
+  presenterl:conditional([
+    workorder_riak:has_binary_index("status", <<"Completed">>, Obj)
+  ], [
+    <<"<tr><td>Status</td><td>Completed</td>">>
+  ], P),
+
+  P ! [
+    <<"</table>\n">>,
+    <<"</body>\n">>,
+    <<"</html>\n">>
+  ],
+
+  Body = presenterl:encode(P),
+
+  {Body, Req, State}.
