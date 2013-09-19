f7c8076812384b48624c9197a34f4600bfd5cb09
Phil Harvey <philharveyx@gmail.com>
2013-09-19 10:36:02 -0500

    Implement empty work order collection

--- src/workorder_root_handler.erl
+++ src/workorder_root_handler.erl
@@ -1,3 +1,17 @@
 -module(workorder_root_handler).
 
+-export([init/3]).
+-export([content_types_provided/2]).
+-export([work_order_collection_to_json/2]).
 
+init(_Transport, _Req, []) ->
+  {upgrade, protocol, cowboy_rest}.
+
+content_types_provided(Req, State) ->
+  {[
+    {<<"application/json">>, work_order_collection_to_json}
+  ], Req, State}.
+
+work_order_collection_to_json(Req, State) ->
+  Body = <<"{}">>,
+  {Body, Req, State}.
